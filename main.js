e177e2b626bf0d7332896878ac444c116302db32
manit20 <78215866+manit20@users.noreply.github.com>
2021-07-16 18:45:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function preload(){}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.position(490,85);
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+}
+
+function modelLoaded(){
+  console.log("Model Loaded");
+}
+
+function draw(){
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResults);
+}
+
+function gotResults(error, results){
+  if(error){
+    console.error(error);
+  }
+  else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
+
